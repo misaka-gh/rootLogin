3c354530dcd8baced070dceb2fded9c0a0085442
Misaka No <96560028+misakano7545@users.noreply.github.com>
2021-12-25 20:42:28 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,6 +4,10 @@ green(){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
 
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
 if [[ -f /etc/redhat-release ]]; then
 release="Centos"
 elif cat /etc/issue | grep -q -E -i "debian"; then
