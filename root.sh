2a0982065a68704e8e5521f15ca2269c54d0edd1
Misaka No <96560028+misakano7545@users.noreply.github.com>
2021-12-25 20:41:52 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,6 +4,37 @@ green(){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
 
+if [[ -f /etc/redhat-release ]]; then
+release="Centos"
+elif cat /etc/issue | grep -q -E -i "debian"; then
+release="Debian"
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+release="Ubuntu"
+elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+release="Centos"
+elif cat /proc/version | grep -q -E -i "debian"; then
+release="Debian"
+elif cat /proc/version | grep -q -E -i "ubuntu"; then
+release="Ubuntu"
+elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+release="Centos"
+else 
+red "不支持你当前系统，请选择使用Ubuntu,Debian,Centos系统。请向作者反馈 https://github.com/kkkyg/CFwarp/issues"
+rm -f root.sh
+exit 1
+fi
+
+if ! type curl >/dev/null 2>&1; then 
+yellow "检测到sudo未安装，安装中 "
+if [ $release = "Centos" ]; then
+yum -y update && yum install sudo -y
+else
+apt update -y && apt install sudo -y
+fi	   
+else
+green " curl已安装，继续 "
+fi
+
 sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
