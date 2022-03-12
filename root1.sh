953144e41fb2f86f72177b7f93da6a031e3f9b0c
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-12 15:59:52 +0800

    Create root1.sh

---
+++ root1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "alpine")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update" "apk update -f")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+for i in "${CMD[@]}"; do
+	SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int=0; int<${#REGEX[@]}; int++)); do
+	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "不支持VPS的当前系统，请使用主流操作系统" && exit 1
+[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} curl
+
+IP=$(curl -sm8 ip.sb)
+
+sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+
+read -p "输入即将设置的SSH端口（如未输入，默认22）：" sshport
+[ -z $sshport ] && red "端口未设置，将使用默认22端口" && sshport=22
+read -p "输入即将设置的root密码：" password
+[ -z $password ] && red "未检测输入，脚本即将退出" && exit 1
+echo root:$password | sudo chpasswd root
+sudo sed -i "s/^#\?Port.*/Port $sshport/g" /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
+yellow "VPS root登录信息设置完成！"
+green "VPS登录地址：$IP:$sshport"
+green "用户名：root"
+green "密码：$password"
+yellow "请妥善保存好登录信息！然后重启VPS确保设置已保存！"
