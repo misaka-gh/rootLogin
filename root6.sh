520b9ed31a0bd3023c01e91b0e8fd2a79287683a
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-12 11:03:23 +0800

    Update root6.sh

--- root6.sh
+++ root6.sh
@@ -41,7 +41,7 @@ read -p "输入即将设置的SSH端口（如未输入，默认22）：" sshport
 read -p "输入即将设置的root密码：" password
 [ -z $password ] && red "未检测输入，脚本即将退出" && exit 1
 echo root:$password | sudo chpasswd root
-sudo sed -i 's/^#\?Port.*/Port $sshport/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?Port.*/Port \$sshport/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service ssh restart
